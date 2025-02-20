1706e152b608e6ee50b058876a8e07c4799b051f
kameronr <kameronr@elmersmfg.com>
2025-02-20 12:01:02 -0600

    input board updates

--- build.sh
+++ build.sh
@@ -2,6 +2,25 @@
 
 set -e # exit on errors
 
+# Parse command line arguments for HOST and PORT
+while getopts "h:p:" opt; do
+  case $opt in
+    h) HOST=$OPTARG ;;
+    p) PORT=$OPTARG ;;
+    \?) echo "Usage: $0 [-h HOST] [-p PORT]" && exit 1 ;;
+  esac
+done
+
+# Check if HOST and PORT are provided, otherwise set default values
+HOST=${HOST:-localhost}    # Default to 'localhost' if not provided
+PORT=${PORT:-8000}         # Default to 8000 if not provided
+
+# Set environment variables for React and Vite
+export REACT_APP_HOST=$HOST
+export REACT_APP_PORT=$PORT
+export VITE_HOST=$HOST
+export VITE_PORT=$PORT
+
 # Define the list of directories to build
 directories=("FrontEndSimulation" "input-board")
 
